4de0ebe4d864e36df022e35900ed1a5d98e1b6c7
raylinhonghu <raylinhonghu@gmail.com>
2018-01-27 00:49:46 -0800

    midContent btn add

--- js/nav.js
+++ js/nav.js
@@ -1,19 +1,54 @@
 $(document).ready(function(){
-  var name1 = $('.menu-unclicked');
+  var unclickedItems = $('.menu-unclicked');
+
   // menu interaction
   $(".menu-unclicked").on("click",function(){
+    var clickedName = $(this).attr("name");
+
     // hide and show except this clicked
-    $.each(name1,function(i,val) {
-      console.log(val);
-      if($(val) != $(this))
-        $(val).show(300);
-        $(val).next().hide(300);
+    $.each(unclickedItems,function(index,eachItem) {
+      if($(eachItem) != $(this))
+        $(eachItem).show(300);
+        $(eachItem).next().hide(300);
+
     })
+
     // this. hide and show
     $(this).hide(300);
     $(this).next().show('hide-navContent');
+
+    // midContent Display
+    switch (clickedName) {
+      case  "home":
+        $('.home').show(0);$('.honghulin').hide(0);
+        $('.projects').hide(0);$('.blogs').hide(0);
+        $('.tags').hide(0);
+        break;
+      case  "honghulin":
+        $('.honghulin').show(0);$('.home').hide(0);
+        $('.projects').hide(0);$('.blogs').hide(0);
+        $('.tags').hide(0);
+        break;
+      case  "projects":
+        $('.projects').show(0);$('.honghulin').hide(0);
+        $('.home').hide(0);$('.blogs').hide(0);
+        $('.tags').hide(0);
+        break;
+      case  "blogs":
+        $('.blogs').show(0);$('.honghulin').hide(0);
+        $('.home').hide(0);$('.projects').hide(0);
+        $('.tags').hide(0);
+        break;
+      case  "tags":
+        $('.tags').show(0);$('.honghulin').hide(0);
+        $('.home').hide(0);$('.projects').hide(0);
+        $('.blogs').hide(0);
+        break;
+    }
+
   });
 
+  // click red navigation
   $("nav a").on("click",function(){
     var hideNav = $(this).parent().parent();
     hideNav.toggle('hide-navContent');
@@ -28,16 +63,16 @@ $(document).ready(function(){
 
 
   // GET MID Content
-  $.ajax({
-    type:"GET",
-    url: "https://learnwebcode.github.io/json-example/animals-2.json",
-    success:function(data){
-      console.log(data[0].name);
-    },
-    error:function(jqXHR,textStatus,error){
-      console.log(error);
-    }
-  })
- 
+  // $.ajax({
+  //   type:"GET",
+  //   url: "https://github.com/raylinhonghu/MyWebsite/blob/master/json/midContent.json",
+  //   success:function(data){
+  //     console.log(data[0].name);
+  //   },
+  //   error:function(jqXHR,textStatus,error){
+  //     console.log(error);
+  //   }
+  // })
+
 
 });
